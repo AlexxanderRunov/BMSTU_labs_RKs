6c19cccc8e37fad87c574953dabd2937d264c31c
Alexander Runov <aleksandrrunov02@gmail.com>
2023-12-06 00:52:16 +0300

    C# Labs
    labs 2 & 3

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+public abstract class GeometricFigure
+{
+    // Виртуальный метод для вычисления площади фигуры
+    public abstract double CalculateArea();
+
+    public override string ToString()
+    {
+        return $"Площадь фигуры: {CalculateArea()}";
+    }
+}
+
+public class Rectangle : GeometricFigure
+{
+    // Ширина и высота объявляются как свойства
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    // Конструктор по параметрам «ширина» и «высота»
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Прямоугольник: Ширина = {Width}, Высота = {Height}. {ToString()}");
+    }
+}
+
+public class Square : Rectangle
+{
+    //  Конструктор по длине стороны
+    public Square(double side) : base(side, side)
+    {
+    }
+
+    public new void Print()
+    {
+        Console.WriteLine($"Квадрат: Сторона = {Width}. {ToString()}");
+    }
+}
+
+public class Circle : GeometricFigure
+{
+    // Радиус объявляется как свойство
+    public double Radius { get; set; }
+
+    // Конструктор по параметру «радиус»
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Круг: Радиус = {Radius}. {ToString()}");
+    }
+}
+
+public interface IPrint
+{
+    void Print();
+}
+
+class Program
+{
+    static void Main()
+    {
+        Rectangle rectangle = new Rectangle(5, 10);
+        Square square = new Square(7);
+        Circle circle = new Circle(3);
+
+        rectangle.Print();
+        square.Print();
+        circle.Print();
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,86 @@
+﻿// Интерфейс IComparable
+interface IComparable
+{
+    double GetArea();
+}
+
+// Базовый класс "Геометрическая фигура"
+class Figure : IComparable, IComparer<Figure>
+{
+    public virtual double GetArea()
+    {
+        return 0.0;
+    }
+
+    public int CompareTo(object obj)
+    {
+        Figure other = obj as Figure;
+        if (other == null)
+            throw new ArgumentException("Object is not a Figure");
+
+        return this.GetArea().CompareTo(other.GetArea());
+    }
+
+    public int Compare(Figure x, Figure y)
+    {
+        return x.GetArea().CompareTo(y.GetArea());
+    }
+}
+
+// Производные классы: Прямоугольник, Квадрат, Круг
+class Rectangle : Figure
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Square : Figure
+{
+    public double Side { get; set; }
+
+    public override double GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Circle : Figure
+{
+    public double Radius { get; set; }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Figure> figureList = new List<Figure>
+        {
+            new Rectangle { Width = 6, Height = 8 },
+            new Square { Side = 4 },
+            new Circle { Radius = 3.0 }
+        };
+
+        figureList.Sort(new Figure());
+        Console.WriteLine("\nList<Figure> sorted by area:");
+        PrintCollection(figureList);
+    }
+
+    // Вывод элементов коллекции
+    static void PrintCollection(List<Figure> collection)
+    {
+        foreach (var item in collection)
+        {
+            Console.WriteLine($"Figure: {item.GetType().Name}, Area: {((Figure)item).GetArea()}");
+        }
+    }
+}
